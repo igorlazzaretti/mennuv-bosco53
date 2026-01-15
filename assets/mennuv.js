517a300a341782f64d2c35b6fb2700f75b233693
igor-lazzaretti <idlazzaretti@gmail.com>
2026-01-15 14:14:24 -0300

    :tada: Mennuv - Bosto 53 cm: mais videos para teste piloto

--- assets/mennuv.js
+++ assets/mennuv.js
@@ -2,31 +2,31 @@ const categoriasData = [
     {
         id: 'tradicionais',
         nome: 'Doces Finos de Pelotas',
-        icone: 'editor_choice', 
+        icone: 'editor_choice',
         descricao: 'A nobreza da autêntica tradição doceira, feita com receitas centenárias.'
     },
     {
         id: 'salgados',
         nome: 'Salgados, Lanches, Pizzas, Sopas e Saladas',
-        icone: 'bakery_dining', 
+        icone: 'bakery_dining',
         descricao: 'Gastronomia artesanal e ingredientes frescos, do lanche rápido ao jantar leve.'
     },
     {
         id: 'tortas',
         nome: 'Tortas, Bolos e Sobremesas',
-        icone: 'cake', 
+        icone: 'cake',
         descricao: 'Confeitaria fina e sabores inesquecíveis para celebrar cada momento.'
     },
     {
         id: 'cafes',
         nome: 'Cafés, Cafés Gelados, Expressos e Elaborados',
-        icone: 'coffee', 
+        icone: 'coffee',
         descricao: 'Grãos premium e a arte do barista em extrações perfeitas.'
     },
     {
         id: 'bebidas',
         nome: 'Bebidas Quentes e de Verão',
-        icone: 'local_bar', 
+        icone: 'local_bar',
         descricao: 'Do conforto dos chás aromáticos ao frescor dos drinks exclusivos.'
     }
 ];
@@ -37,7 +37,14 @@ const produtosData = [
         nome: 'Bombom de Morango',
         preco: 'R$ 9,50',
         descricao: 'O frescor da fruta selecionada envolto em doce artesanal, criando um contraste perfeito de sabores.',
-        imagem: 'https://static.saipos.com/saipos-estatico/img-items/63093/store_item/a74cba6f3a1350e2b905f5ebad4793f1.png',
+        src: './assets/videos/bombom.mp4',
+    },
+    {
+        categoria: 'tradicionais',
+        nome: 'Bombom de Morango',
+        preco: 'R$ 9,50',
+        descricao: 'O frescor da fruta selecionada envolto em doce artesanal, criando um contraste perfeito de sabores.',
+        src: './assets/videos/bombom.mp4',
     },
     {
         categoria: 'tradicionais',
@@ -65,54 +72,72 @@ const produtosData = [
         nome: 'Ninho',
         preco: 'R$ 9,50',
         descricao: 'Obra de arte conventual: delicados fios de ovos abraçando um recheio cremoso de ovos moles.',
+        imagem: 'https://static.saipos.com/saipos-estatico/img-items/63093/store_item/3495ca7525dbd55e2081e5090d8478a2.png'
+
     },
     {
         categoria: 'tradicionais',
         nome: 'Quindim Tradicional',
         preco: 'R$ 9,50',
         descricao: 'O ícone de Pelotas. Brilho espelhado, base de coco e interior cremoso de gemas frescas.',
+        imagem: 'https://static.saipos.com/saipos-estatico/img-items/63093/store_item/3495ca7525dbd55e2081e5090d8478a2.png'
+
     },
     {
         categoria: 'tradicionais',
         nome: 'Quindim de Leite Condensado',
         preco: 'R$ 9,50',
         descricao: 'Uma variação suave e sedosa do clássico, com textura que remete ao mais fino pudim.',
+        imagem: 'https://static.saipos.com/saipos-estatico/img-items/63093/store_item/3495ca7525dbd55e2081e5090d8478a2.png'
+
     },
     {
         categoria: 'tradicionais',
         nome: 'Quindim Recheado com Nozes',
         preco: 'R$ 9,50',
         descricao: 'A fusão perfeita: o quindim dourado revelando um coração surpreendente de doce de nozes.',
+        imagem: 'https://static.saipos.com/saipos-estatico/img-items/63093/store_item/3495ca7525dbd55e2081e5090d8478a2.png'
+
     },
     {
         categoria: 'tradicionais',
         nome: 'Quindim ao Café',
         preco: 'R$ 9,50',
         descricao: 'Sofisticação para o paladar adulto. A doçura tradicional equilibrada com notas de café torrado.',
+        imagem: 'https://static.saipos.com/saipos-estatico/img-items/63093/store_item/3495ca7525dbd55e2081e5090d8478a2.png'
+
     },
     {
         categoria: 'tradicionais',
         nome: 'Quindim de Nozes',
         preco: 'R$ 9,50',
         descricao: 'Massa especial enriquecida com nozes, conferindo textura única e sabor amendoado.',
+        imagem: 'https://static.saipos.com/saipos-estatico/img-items/63093/store_item/3495ca7525dbd55e2081e5090d8478a2.png'
+
     },
     {
         categoria: 'tradicionais',
         nome: 'Trouxinha de Fios de Ovos',
         preco: 'R$ 9,50',
         descricao: 'Exuberantes fios de ovos dourados, levemente gratinados para realçar o sabor.',
+        imagem: 'https://static.saipos.com/saipos-estatico/img-items/63093/store_item/3495ca7525dbd55e2081e5090d8478a2.png'
+
     },
     {
         categoria: 'tradicionais',
         nome: 'Trouxinha de Nozes',
         preco: 'R$ 9,50',
         descricao: 'A leveza dos fios de ovos envolvendo um recheio denso e saboroso de nozes nobres.',
+        imagem: 'https://static.saipos.com/saipos-estatico/img-items/63093/store_item/3495ca7525dbd55e2081e5090d8478a2.png'
+
     },
     {
         categoria: 'tradicionais',
         nome: 'Ninho Glaceado',
         preco: 'R$ 9,50',
         descricao: 'A elegância do Ninho tradicional coberto por uma fina camada de glacê acetinado.',
+        imagem: 'https://static.saipos.com/saipos-estatico/img-items/63093/store_item/3495ca7525dbd55e2081e5090d8478a2.png'
+
     },
     // Salgados, Lanches, Pizzas, Sopas e Saladas
     {
@@ -123,6 +148,20 @@ const produtosData = [
         src: './assets/videos/bruchet.mp4',
     },
     {
+        categoria: 'salgados',
+        nome: 'Bruschetta ',
+        preco: 'R$ 25,00',
+        descricao: 'Clássica bruschetta com tomates e manjericão fresco.',
+        src: './assets/videos/bruchet.mp4',
+    },
+    {
+        categoria: 'salgados',
+        nome: 'Prato de Batata Frita',
+        preco: 'R$ 13,90',
+        descricao: 'Batata frita com molho especial.',
+        src: './assets/videos/batata.mp4',
+    },
+        {
         categoria: 'salgados',
         nome: 'Quiche de Milho com Bacon',
         preco: 'R$ 13,90',
@@ -144,19 +183,63 @@ const produtosData = [
         descricao: 'Fatia de torta de limão com chocolate amargo.',
         src: './assets/videos/Vídeo_de_Torta_de_Limão_e_Chocolate.webm',
     },
+    {
+        categoria: 'tortas',
+        nome: 'Fatia de Torta de Limão com Chocolate Amargo',
+        preco: 'R$ 17,00',
+        descricao: 'Fatia de torta de limão com chocolate amargo.',
+        src: './assets/videos/Vídeo_de_Torta_de_Limão_e_Chocolate.webm',
+    },
+    {
+        categoria: 'tortas',
+        nome: 'Fatia de Torta de Limão com Chocolate Amargo',
+        preco: 'R$ 17,00',
+        descricao: 'Fatia de torta de limão com chocolate amargo.',
+        src: './assets/videos/Vídeo_de_Torta_de_Limão_e_Chocolate.webm',
+    },
     // Cafés, Cafés Gelados, Expressos e Elaborados
     {
         categoria: 'cafes',
         nome: 'Cappuccino Italiano',
         preco: 'R$ 14,00',
         descricao: 'Espresso, leite vaporizado e espuma de leite, finalizado com cacau em pó.',
+        src: './assets/videos/capucino.mp4',
+    },
+    {
+        categoria: 'cafes',
+        nome: 'Cappuccino Italiano',
+        preco: 'R$ 14,00',
+        descricao: 'Espresso, leite vaporizado e espuma de leite, finalizado com cacau em pó.',
+        src: './assets/videos/capucino.mp4',
+    },
+    {
+        categoria: 'cafes',
+        nome: 'Cappuccino Italiano',
+        preco: 'R$ 14,00',
+        descricao: 'Espresso, leite vaporizado e espuma de leite, finalizado com cacau em pó.',
+        src: './assets/videos/capucino.mp4',
     },
     // Bebidas Quentes e de Verão
     {
-        categoria: 'sazonais',
-        nome: 'Panetone Trufado',
-        preco: 'R$ 85,00',
+        categoria: 'bebidas',
+        nome: 'Drink de Amora',
+        preco: 'R$ 15,00',
         descricao: 'Massa de fermentação natural com gotas de chocolate belga e recheio de trufa.',
+        src: './assets/videos/drink.mp4',
+    },
+    {
+        categoria: 'bebidas',
+        nome: 'Drink de Amora',
+        preco: 'R$ 15,00',
+        descricao: 'Massa de fermentação natural com gotas de chocolate belga e recheio de trufa.',
+        src: './assets/videos/drink.mp4',
+    },
+    {
+        categoria: 'bebidas',
+        nome: 'Drink de Amora',
+        preco: 'R$ 15,00',
+        descricao: 'Massa de fermentação natural com gotas de chocolate belga e recheio de trufa.',
+        src: './assets/videos/drink.mp4',
     }
 ];
 
